4563d9ebd0cee41a41b1e22e04f0491691531c75
McDonald75 <mcdonald.omondi@student.moringaschool.com>
2024-12-02 13:51:04 +0000

    complete

--- lib/dog.py
+++ lib/dog.py
@@ -12,4 +12,24 @@ APPROVED_BREEDS = [
 ]
 
 class Dog:
-    pass
+    name = "Scoop"
+    breed = "Pug"
+    def __init__(self, name = "Scoop", breed = "Pug"):
+        self.set_name(name)
+        self.set_breed(breed)
+    def set_name(self, name):
+        if type(name) == str and name != '' and len(name) < 26:
+            self._name = name
+        else:
+            print("Name must be string between 1 and 25 characters.")
+    def get_name(self):
+        return self._name
+    def set_breed(self, breed):
+        if(breed in APPROVED_BREEDS):
+            self._breed = breed
+        else:
+            print("Breed must be in list of approved breeds.")
+    def get_breed(self):
+        return self._breed
+    name = property(get_name, breed)
+    breed = property(get_breed, breed)
